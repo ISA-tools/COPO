cfa979cac7c7f4c477d6cc8f707b33c167debc1a
Felix Shaw <felix.shaw@tgac.ac.uk>
2015-04-22 15:03:01 +0100

    adding option to setup file for mysql-python

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,7 +8,7 @@ sudo pip install markdown
 sudo pip install django-filter
 sudo pip install --allow-all-external mysql-connector-python
 sudo pip install mongokit
-sudo pip install mysql-python
+sudo pip install mysql-connector-python --allow-external mysql-connector-python
 sudo pip install jsonpickle
 sudo pip install elementtree --allow-external elementtree --allow-unverified elementtree
 sudo pip install lxml
@@ -24,18 +24,9 @@ brew install mongodb
 
 sudo chown -R _mysql /usr/local/var/mysql
 sudo mysql.server start
-sh ./createdb.sh copo_development fshaw Apple123
+sh ./createdb.sh copo_development alfie password
 
 sudo mkdir /data
 sudo mkdir /data/db
 mongod
 
-# bodge needed to getg migrations working
-# go into web_copo/migrations and find initial migration, then comment out the line in dependencies
-# ('chunked_upload', '0001_initial'),
-# sudo manage.py syncdb
-# sudo manage.py makemigrations chunked_upload
-# uncommment chunked_upload
-# sudo manage.py makemigrations
-# then perform 'python manage.py makemigrations'
-# uncomment and rerun, then migrate
