d1fa8ee32dfd23a911573adca7aad6c2c645dfb4
Felix Shaw <felix.shaw@tgac.ac.uk>
2015-04-24 12:15:39 +0100

    changing settings file in manage.py

--- web/manage.py
+++ web/manage.py
@@ -4,7 +4,7 @@ import sys
 import subprocess
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spax.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", “project_copo.settings.settings")
 
     script_pth = os.path.join(os.path.dirname(os.path.abspath(__file__)), "managedependencies.py")
 
