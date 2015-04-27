c972e5c97e7bce3985abff9b21c43edd0ea515ff
tonietuk <tonietuk@me.com>
2015-04-27 11:44:04 +0100

    added housekeeping for mongodb

--- web/manage.py
+++ web/manage.py
@@ -4,7 +4,7 @@ import sys
 import subprocess
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spax.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_copo.settings.settings")
 
     script_pth = os.path.join(os.path.dirname(os.path.abspath(__file__)), "managedependencies.py")
 
--- web/managedependencies.py
+++ web/managedependencies.py
@@ -6,19 +6,35 @@ import subprocess
 proc = subprocess.Popen(["mysqladmin", "-umysql", "ping"], stdout=subprocess.PIPE)
 out, err = proc.communicate()
 if not err:
-	if "alive" in out.decode("utf-8").lower():
-		print("MySQL is running.")
-	else:
-		print("Attempting to start MySQL server...")
-		subprocess.call(["mysql.server", "start"])
-
+    if "alive" in out.decode("utf-8").lower():
+        print("MySQL is running.")
+    else:
+        print("Attempting to start MySQL server...")
+        subprocess.call(["mysql.server", "start"])
+else:
+    print("MySQL - Cannot determine server's status.")
 
 # check and start redis server if not running
 proc = subprocess.Popen(["redis-cli", "ping"], stdout=subprocess.PIPE)
 out, err = proc.communicate()
 if not err:
-	if "pong" in out.decode("utf-8").lower():
-		print("Redis is running.")
-	else:
-		print("Attempting to start Redis server...")
-		subprocess.call(["redis-server", "/usr/local/etc/redis.conf"])
+    if "pong" in out.decode("utf-8").lower():
+        print("Redis is running.")
+    else:
+        print("Attempting to start Redis server...")
+        subprocess.call(["redis-server", "/usr/local/etc/redis.conf"])
+else:
+    print("Redis - Cannot determine server's status.")
+
+# check and start mongo db if not running
+proc = subprocess.Popen(["pgrep","mongod"], stdout=subprocess.PIPE)
+out, err = proc.communicate()
+if not err:
+    if len(out.decode("utf-8")) > 0:
+        print("Mongo is running.")
+    else:
+        print("Attempting to start MongoDB...")
+        status = subprocess.call("mongod --fork --logpath ~/logs/mongo/mongodb.log", shell=True)
+else:
+    print("Mongo - Cannot determine server's status.")
+
