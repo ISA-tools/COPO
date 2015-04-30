28b112c358bff887d1112f1ec7bfc1ae5f8121a2
tonietuk <tonietuk@me.com>
2015-04-30 12:15:28 +0100

    dependency test

--- web/managedependencies.py
+++ web/managedependencies.py
@@ -1,31 +1,39 @@
-#! /usr/bin/python
+#!/usr/bin/env python
 
 import subprocess
 
+
 # check and start mysql server if not running
-proc = subprocess.Popen(["mysqladmin", "-umysql", "ping"], stdout=subprocess.PIPE)
+proc = subprocess.Popen(["pgrep","mysql"], stdout=subprocess.PIPE)
 out, err = proc.communicate()
 if not err:
-    if "alive" in out.decode("utf-8").lower():
+    if len(out.decode("utf-8")) > 0:
         print("MySQL is running.")
     else:
         print("Attempting to start MySQL server...")
-        subprocess.call(["mysql.server", "start"])
+        print(subprocess.call(["which","mysql"]))
+        # proc1 = subprocess.Popen(["which", "mysql"], stdout=subprocess.PIPE)
+        # proc1 = subprocess.Popen('which mysql', executable='/usr/bin/sh', shell=True)
+        # out, err = proc1.communicate()
+        # pth = proc1.stdout.read().decode("utf-8").strip()
+        # subprocess.call([pth+".server", "start"])
 else:
     print("MySQL - Cannot determine server's status.")
 
 # check and start redis server if not running
-proc = subprocess.Popen(["redis-cli", "ping"], stdout=subprocess.PIPE)
-out, err = proc.communicate()
-if not err:
-    if "pong" in out.decode("utf-8").lower():
-        print("Redis is running.")
-    else:
-        print("Attempting to start Redis server...")
-        subprocess.call(["redis-server", "/usr/local/etc/redis.conf"])
-else:
-    print("Redis - Cannot determine server's status.")
-
+# proc = subprocess.Popen(["pgrep","redis-server"], stdout=subprocess.PIPE)
+# out, err = proc.communicate()
+# if not err:
+#     if len(out.decode("utf-8")) > 0:
+#         print("Redis is running.")
+#     else:
+#         print("Attempting to start Redis server...")
+#         proc1 = subprocess.Popen(["which", "redis-server"], stdout=subprocess.PIPE)
+#         pth = proc1.stdout.read().decode("utf-8").strip()
+#         subprocess.call([pth, "/usr/local/etc/redis.conf"])
+# else:
+#     print("Redis - Cannot determine server's status.")
+#
 # check and start mongo db if not running
 proc = subprocess.Popen(["pgrep","mongod"], stdout=subprocess.PIPE)
 out, err = proc.communicate()
@@ -34,7 +42,18 @@ if not err:
         print("Mongo is running.")
     else:
         print("Attempting to start MongoDB...")
-        status = subprocess.call("mongod --fork --logpath ~/logs/mongo/mongodb.log", shell=True)
+        # cmd = "which mongod"
+        # kk = subprocess.Popen(['/bin/bash', '-c', cmd])
+        # proc1 = subprocess.Popen(["which", "mongod"], stdout=subprocess.PIPE)
+        # out, err = kk.communicate()
+        #pth = proc1.stdout.read().decode("utf-8").strip()
+        #cmd = pth+" --fork --logpath ~/logs/mongo/mongodb.log"
+        print(out)
+        # subprocess.Popen(['/bin/bash', '-c', cmd])
+        #subprocess.call([pth, " --fork --logpath ~/logs/mongo/mongodb.log"])
+        #subprocess.call([pth])
+        # status = subprocess.call(pth, shell=True)
+        #status = subprocess.call(pth+" --fork --logpath ~/logs/mongo/mongodb.log", shell=True)
 else:
     print("Mongo - Cannot determine server's status.")
 
