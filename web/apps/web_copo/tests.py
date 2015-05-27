ab2225d4ee50d456e483f0b0e6b0b76259722718
Felix Shaw <felix.shaw@tgac.ac.uk>
2015-05-27 12:56:10 +0100

    adding unit tests for mongodb

--- web/apps/web_copo/tests.py
+++ web/apps/web_copo/tests.py
@@ -1,17 +1,32 @@
 from django.test import TestCase
-#from web.test import Client
-import unittest
-#from selenium import webdriver
+import pymongo
+
+from project_copo.settings.settings import *
+
+
+def get_collection_ref(collection_name):
+    return pymongo.MongoClient(settings.MONGO_HOST, settings.MONGO_PORT)[settings.MONGO_DB][collection_name]
+
 
 # Create your tests here.
 
-class EnaTest(unittest.TestCase):
+class EnaTest(TestCase):
     def setUp(self):
-        pass
+        return get_collection_ref("Unit_Test_Collection")
 
 
-    def test_experiment(self):
-        #driver = webdriver.Firefox()
-        #driver.get("/copo")
-        self.assertEqual(200, 200)
+    def setUp(self):
+        self.db = get_collection_ref('test')
+
+
+    def test_doc_published(self):
+        # Set up a document to save
+        doc = dict(text="test",
+                      user_id=1)
+        self.db.insert(doc)
+        self.assertEqual(self.db.find_one(
+            {'user_id':1})['text'], 'test')
+
+    def tearDown(self):
+        self.db.drop()
 
