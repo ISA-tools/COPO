46988b1ff1667db2dfc92f858d7bb4e542644df6
Felix Shaw <felix.shaw@tgac.ac.uk>
2015-06-26 12:17:06 +0100

    moving allauth packed into copo sources

--- web/apps/web_copo/mongo/orcid_da.py
+++ web/apps/web_copo/mongo/orcid_da.py
@@ -1,7 +1,8 @@
 __author__ = 'felix.shaw@tgac.ac.uk - 24/06/15'
 
 
-from apps.web_copo.mongo.mongo_util import *
+from apps.web_copo.mongo.mongo_util import get_collection_ref
+from allauth.socialaccount.models import SocialAccount
 
 ORCID = get_collection_ref("OcidCollections")
 
@@ -11,6 +12,7 @@ class Orcid:
     def store_orcid_profile(self, profile_data, user):
 
         user_id = user.id
-        #ORCID.insert()
+        social_account = SocialAccount.objects.get(user_id=user_id)
+        profile_data = social_account.extra_data
         print(profile_data)
         print(user_id)
