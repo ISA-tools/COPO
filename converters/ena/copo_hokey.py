4a8b024ef37250ee4b1d0bae4716b50cde5a9023
Felix Shaw <felix.shaw@tgac.ac.uk>
2015-09-15 11:15:20 +0100

    adding in functionality for assay -> experiment mapping in SRA

--- converters/ena/copo_hokey.py
+++ converters/ena/copo_hokey.py
@@ -12,6 +12,8 @@ class exporter(base_exporter):
         from bson import ObjectId
         import xml.etree.ElementTree as ET
         from django_tools.middlewares import ThreadLocal
+        import uuid
+        from dateutil.parser import parse
 
 
         head = copo_base_da.Collection_Head().GET(ObjectId(collection_id))
@@ -42,7 +44,7 @@ class exporter(base_exporter):
 
         # write to file
         tree = ET.ElementTree(STUDY_SET)
-        tree.write('/Users/fshaw/Desktop/study.xml')
+        tree.write('/Users/fshaw/Desktop/xmlout/study.xml')
 
 
         # create samples.xml
@@ -52,6 +54,8 @@ class exporter(base_exporter):
         study_counter = 0
         # must iterate all isa studies to extract sample list from each
         for study in c['studies']:
+
+            study_date = parse(study['study']['studySubmissionDate'])
 
             for s in study['study']['studySamples']:
 
@@ -82,12 +86,11 @@ class exporter(base_exporter):
                 DESCRIPTION = ET.SubElement(SAMPLE, "DESCRIPTION")
                 DESCRIPTION.text = source_name
 
-            # create experiment.xml
-            EXPERIMENT_SET = ET.Element("ELEMENT_SET")
+
 
             assay_counter = 0
             for a in study['study']['assays']:
-
+                tech_type = a['studyAssayMeasurementType']
 
                 # iterate through assay table to get required info
 
@@ -100,11 +103,13 @@ class exporter(base_exporter):
                 '''
 
                 exp_run = {}
+                exp_title = (a['studyAssayMeasurementType'] + ' (Auto-gen)').replace(' ', '_')
                 for row in a['assaysTable']:
                     e = {'datafiles': [], 'multiplexed': False}
                     for index, cell in enumerate(row):
 
-                        datafiles = []
+                        #datafiles = []
+
                         if 'name' in cell and cell['name'] == 'Sample Name':
                             experiment_sample = cell['value']
 
@@ -124,9 +129,11 @@ class exporter(base_exporter):
                                         e['library_selection'] = str(value).upper()
                                     elif term == 'library layout':
                                         e['library_layout'] = str(value).upper()
+                                    elif term == 'mid':
+                                        # assuming that if a barcode is present, this is a multiplexed read
+                                        e['multiplexed'] = True
 
                         if 'name' in cell and cell['name'] == 'Raw Data File' and cell['type'] == 'isaDataNode':
-
                             e['datafiles'].append(cell['value'])
 
                         if 'name' in cell and cell['name'] == 'Protocol REF' and cell['type'] == 'isaProtocolExecutionNode' and cell['value'] == "nucleic acid sequencing":
@@ -139,6 +146,9 @@ class exporter(base_exporter):
                                         ins = value.split(' ', 1)
                                         e['platform'] = ins[0]
                                         e['model'] = ins[1]
+                                    elif term == "quality scorer":
+                                        e['scorer'] = value
+
 
 
                     if experiment_sample not in exp_run:
@@ -148,55 +158,109 @@ class exporter(base_exporter):
                         # if they are, then it is simply an experimental repitition and can be recorded
                         # as an additional run. If there are experimental changes then another
                         # experiment/run pair must be created
-                        for f in e['datafiles']:
-                            exp_run[experiment_sample]['datafiles'].append(f)
-                        exp_run[experiment_sample]['multiplexed'] = True
+                        dup = exp_run[experiment_sample]
+                        same = True
 
-
-
-                    '''
-                    EXPERIMENT = ET.SubElement(EXPERIMENT_SET, "EXPERIMENT")
-                    alias = ('EXP_AUTOGEN_' + a['studyAssayTechnologyType'] + '_' + str(study_counter) + '_' + str(assay_counter)).replace(' ', '_')
-
-                    EXPERIMENT.set('alias', alias)
-                    EXPERIMENT.set('center_name', affilation)
-                    EXPERIMENT_TITLE = ET.SubElement(EXPERIMENT, 'TITLE')
-                    exp_title = (a['studyAssayMeasurementType'] + ' (Auto-gen)').replace(' ', '_')
-                    EXPERIMENT_TITLE.text = exp_title
-                    STUDY_REF = ET.SubElement(EXPERIMENT, 'STUDY_REF')
-                    STUDY_REF.set('refcenter', affilation)
-                    STUDY_REF.set('refname', study_alias)
-
-                    DESIGN = ET.SubElement(EXPERIMENT, 'DESIGN')
-                    DESIGN_DESCRIPTION = ET.SubElement(DESIGN, 'DESIGN_DESCRIPTION')
-                    DESIGN_DESCRIPTION.text = study_alias
-                    SAMPLE_DESCRIPTOR = ET.SubElement(DESIGN, 'SAMPLE_DESCRIPTOR')
-                    SAMPLE_DESCRIPTOR.set('refname', experiment_sample)
-                    SAMPLE_DESCRIPTOR.set('refcentre', affilation)
-                    LIBRARY_DESCRIPTOR = ET.SubElement(DESIGN, "LIBRARY_DESCRIPTOR")
-                    LIBRARY_NAME = ET.SubElement(LIBRARY_DESCRIPTOR, "LIBRARY_NAME")
-                    LIBRARY_STRATEGY = ET.SubElement(LIBRARY_DESCRIPTOR, 'LIBRARY_STRATEGY')
-                    LIBRARY_STRATEGY.text = library_stategy
-                    LIBRARY_SOURCE = ET.SubElement(LIBRARY_DESCRIPTOR, "LIBRARY_SOURCE")
-                    LIBRARY_SOURCE.text = library_source
-                    LIBRARY_SELECTION = ET.SubElement(LIBRARY_DESCRIPTOR, "LIBRARY_SELECTION")
-                    LIBRARY_SELECTION.text = library_selection
-                    LIBRARY_LAYOUT = ET.SubElement(LIBRARY_DESCRIPTOR, "LIBRARY_LAYOUT")
-                    TYPE = ET.SubElement(LIBRARY_LAYOUT, library_layout)
-
+                        for key, value in dup.items():
+                            if key != 'datafiles' and e[key] != value:
+                                same = False
+                                break
+                        if same:
+                            for f in e['datafiles']:
+                                exp_run[experiment_sample]['datafiles'].append(f)
+                        else:
+                            exp_run[experiment_sample + '_' + str(assay_counter)] = e
 
                     assay_counter += 1
-                    '''
+                study_counter += 1
+
+            # create experiment xml
+            EXPERIMENT_SET = ET.Element("ELEMENT_SET")
+
+            # create run xml
+            RUN_SET = ET.Element("RUN_SET")
+
+            # now iterate through run_exp and create the appropriate XML
+            assay_counter = 0
+            for el in exp_run:
+
+
+                EXPERIMENT = ET.SubElement(EXPERIMENT_SET, "EXPERIMENT")
+                alias = ('EXP_AUTOGEN_' + a['studyAssayTechnologyType'] + '_' + str(study_counter) + '_' + str(assay_counter)).replace(' ', '_')
+
+                EXPERIMENT.set('alias', alias)
+                EXPERIMENT.set('center_name', affilation)
+                EXPERIMENT_TITLE = ET.SubElement(EXPERIMENT, 'TITLE')
+
+                EXPERIMENT_TITLE.text = exp_title
+                STUDY_REF = ET.SubElement(EXPERIMENT, 'STUDY_REF')
+                STUDY_REF.set('refcenter', affilation)
+                STUDY_REF.set('refname', study_alias)
+
+                DESIGN = ET.SubElement(EXPERIMENT, 'DESIGN')
+                DESIGN_DESCRIPTION = ET.SubElement(DESIGN, 'DESIGN_DESCRIPTION')
+                DESIGN_DESCRIPTION.text = study_alias
+                SAMPLE_DESCRIPTOR = ET.SubElement(DESIGN, 'SAMPLE_DESCRIPTOR')
+                SAMPLE_DESCRIPTOR.set('refname', experiment_sample)
+                SAMPLE_DESCRIPTOR.set('refcentre', affilation)
+                LIBRARY_DESCRIPTOR = ET.SubElement(DESIGN, "LIBRARY_DESCRIPTOR")
+                LIBRARY_NAME = ET.SubElement(LIBRARY_DESCRIPTOR, "LIBRARY_NAME")
+                LIBRARY_STRATEGY = ET.SubElement(LIBRARY_DESCRIPTOR, 'LIBRARY_STRATEGY')
+                LIBRARY_STRATEGY.text = exp_run[el]['library_stategy']
+                LIBRARY_SOURCE = ET.SubElement(LIBRARY_DESCRIPTOR, "LIBRARY_SOURCE")
+                LIBRARY_SOURCE.text = exp_run[el]['library_source']
+                LIBRARY_SELECTION = ET.SubElement(LIBRARY_DESCRIPTOR, "LIBRARY_SELECTION")
+                LIBRARY_SELECTION.text = exp_run[el]['library_selection']
+                LIBRARY_LAYOUT = ET.SubElement(LIBRARY_DESCRIPTOR, "LIBRARY_LAYOUT")
+                TYPE = ET.SubElement(LIBRARY_LAYOUT, exp_run[el]['library_layout'])
+                # optional <POOLING_STRATEGY> field for multiplexed
+                if exp_run[el]['multiplexed']:
+                    POOLING_STRATEGY = ET.SubElement(LIBRARY_DESCRIPTOR, 'POOLING_STRATEGY')
+                    POOLING_STRATEGY.text = 'multiplexed libraries'
+
+                PLATFORM = ET.SubElement(EXPERIMENT, "PLATFORM")
+                MAKE = ET.SubElement(PLATFORM, exp_run[el]['platform'])
+                MODEL = ET.SubElement(MAKE, 'INSTRUMENT_MODEL')
+                MODEL.text = exp_run[el]['model']
+                PROCESSING = ET.SubElement(EXPERIMENT, "PROCESSING")
+
+                # now create the run file tree
+                RUN = ET.SubElement(RUN_SET, "RUN")
+                RUN.set('alias', str(uuid.uuid4()))
+                RUN.set('run_center', affilation)
+                RUN.set('center_name', affilation)
+                RUN.set('run_date', study_date.isoformat())
+                EXPERIMENT_REF = ET.SubElement(RUN, "EXPERIMENT_REF")
+                EXPERIMENT_REF.set('refcenter', affilation)
+                EXPERIMENT_REF.set('refname', alias)
+
+                DATA_BLOCK = ET.SubElement(RUN, "DATA_BLOCK")
+                FILES = ET.SubElement(DATA_BLOCK, "FILES")
+
+                for f in exp_run[el]['datafiles']:
+                    FILE = ET.SubElement(FILES, 'FILE')
+                    FILE.set('filename', f)
+                    FILE.set('checksum_method', 'MD5')
+                    FILE.set('ascii_offset', '!')
+                    FILE.set('filetype', f[f.find('.')+1:])
+                    FILE.set('quality_scoring_system', e['scorer'])
+                    FILE.set('quality_encoding', 'ascii')
+
+                assay_counter += 1
+
+
+
             study_counter += 1
-
-
 
         # write to file
         tree = ET.ElementTree(SAMPLE_SET)
-        tree.write('/Users/fshaw/Desktop/sample.xml')
+        tree.write('/Users/fshaw/Desktop/xmlout/sample.xml')
 
         tree = ET.ElementTree(EXPERIMENT_SET)
-        tree.write('/Users/fshaw/Desktop/experiment.xml')
+        tree.write('/Users/fshaw/Desktop/xmlout/experiment.xml')
+
+        tree = ET.ElementTree(RUN_SET)
+        tree.write('/Users/fshaw/Desktop/xmlout/run.xml')
 
 
 
